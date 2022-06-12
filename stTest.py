37e0d26686bb936800364138b126f6051b9d98be
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2022-06-12 17:32:47 +0200

    Update stTest.py

--- stTest.py
+++ stTest.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[11]:
-
-
 import streamlit as st
 import pandas as pd
 
@@ -13,11 +7,33 @@ st.write("""
     mit diesem Kram
     """)
 
-pd.read_csv('https://raw.githubusercontent.com/Hezel2000/GeoDataScience/main/data/moessbauer%20standard%20data.csv')
+st.header('test')
 
+df = pd.read_csv('https://raw.githubusercontent.com/Hezel2000/GeoDataScience/main/data/moessbauer%20standard%20data.csv')
 
-# In[ ]:
+st.write(df)
 
+st.sidebar.header('ss')
+st.sidebar.selectbox('Element', ['Si', 'Mg', 'Al'])
 
+fig, ax = plt.subplots()
+ax.scatter([1,2,3,4],[1,2,3,4])
 
+st.pyplot(fig)
+
+st.write('test Bokeh')
+
+from bokeh.plotting import figure
+
+x = [1, 2, 3, 4, 5]
+y = [6, 7, 2, 4, 5]
+
+p = figure(
+     title='simple line example',
+     x_axis_label='x',
+     y_axis_label='y')
+
+p.line(x, y, legend_label='Trend', line_width=2)
+
+st.bokeh_chart(p, use_container_width=True)
 
