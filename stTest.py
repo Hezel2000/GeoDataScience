5777a7b50288de1404091c1b7508bbe0c7ce0a51
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2022-06-12 14:13:41 +0200

    Create stTest.py

---
+++ stTest.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import streamlit as st
+
+st.write("""
+    # Kleiner Test
+    
+    mit diesem Kram
+    """)
+
+st.line_chart([1,2,3,4])
+
