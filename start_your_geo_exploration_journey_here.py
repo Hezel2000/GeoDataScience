ee4b0fa523338674061df9e66078c0747bece0f9
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2022-06-24 17:29:59 +0200

    Update start_your_geo_exploration_journey_here.py

--- start_your_geo_exploration_journey_here.py
+++ start_your_geo_exploration_journey_here.py
@@ -23,7 +23,7 @@ st.subheader('Choose where to go next:')
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.markdown("[![Foo](https://raw.githubusercontent.com/Hezel2000/GeoDataScience/main/icons/flank%20method%small.jpg)](https://hezel2000-flank-method-flank-data-reduction-2ncvvv.streamlitapp.com)")
+    st.markdown("[![Foo](https://raw.githubusercontent.com/Hezel2000/GeoDataScience/main/icons/flank%20method%20small.jpg)](https://hezel2000-flank-method-flank-data-reduction-2ncvvv.streamlitapp.com)")
     
 with col2:
     st.markdown("[![Foo](https://raw.githubusercontent.com/Hezel2000/GeoDataScience/main/icons/flank%20method.jpg)](https://hezel2000-flank-method-flank-data-reduction-2ncvvv.streamlitapp.com)")
